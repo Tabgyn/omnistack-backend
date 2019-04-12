11498747be16a2c0bbf749ca70ed1939b61e26e0
Tiago Azevedo Borges <tabgyn@gmail.com>
2019-04-12 13:54:43 -0300

    eslint

--- src/server.js
+++ src/server.js
@@ -9,8 +9,8 @@ app.use(cors());
 const server = require('http').Server(app);
 const io = require('socket.io')(server);
 
-io.on('connection', socket => {
-  socket.on('connectRoom', box => {
+io.on('connection', (socket) => {
+  socket.on('connectRoom', (box) => {
     socket.join(box);
   });
 });
@@ -18,8 +18,8 @@ io.on('connection', socket => {
 mongoose.connect(
   'mongodb+srv://omnistack:omnistack@cluster0-m8bq0.mongodb.net/omnistack?retryWrites=true',
   {
-    useNewUrlParser: true
-  }
+    useNewUrlParser: true,
+  },
 );
 
 app.use((req, res, next) => {
